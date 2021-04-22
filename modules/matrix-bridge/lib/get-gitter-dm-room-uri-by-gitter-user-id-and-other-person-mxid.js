0a4a913ecf60da95d970573a6c2d90bf9d40d6a8
Eric Eastwood <contact@ericeastwood.com>
2021-04-22 17:09:01 -0500

    We can't use Mongo utils in the frontend

--- modules/matrix-bridge/lib/get-gitter-dm-room-uri-by-gitter-user-id-and-other-person-mxid.js
+++ modules/matrix-bridge/lib/get-gitter-dm-room-uri-by-gitter-user-id-and-other-person-mxid.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const assert = require('assert');
-const mongoUtils = require('gitter-web-persistence-utils/lib/mongo-utils');
 
 function getGitterDmRoomUriByGitterUserIdAndOtherPersonMxid(gitterUserId, otherPersonMxid) {
-  assert(mongoUtils.isLikeObjectId(gitterUserId));
+  assert(gitterUserId);
   assert(otherPersonMxid);
 
   const gitterRoomUri = `matrix/${gitterUserId}/${otherPersonMxid}`;
