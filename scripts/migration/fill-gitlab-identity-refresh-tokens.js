bb95a5026b09e4f78c3c845ba03d68424866a035
Eric Eastwood <contact@ericeastwood.com>
2022-05-17 23:32:43 -0500

    Use older initializeUnorderedBulkOp

--- scripts/migration/fill-gitlab-identity-refresh-tokens.js
+++ scripts/migration/fill-gitlab-identity-refresh-tokens.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const shutdown = require('shutdown');
+const readline = require('readline');
 
 const env = require('gitter-web-env');
 const logger = env.logger;
@@ -11,6 +12,7 @@ const onMongoConnect = require('gitter-web-persistence-utils/lib/on-mongo-connec
 const mongoReadPrefs = require('gitter-web-persistence-utils/lib/mongo-read-prefs');
 const { iterableFromMongooseCursor } = require('gitter-web-persistence-utils/lib/mongoose-utils');
 
+// The number of identities to process at once from the database
 const BATCH_SIZE = 500;
 
 // GitLab.com did the GitLab 15 migration to add expiration to all tokens on
@@ -18,6 +20,12 @@ const BATCH_SIZE = 500;
 // this migration, see https://gitlab.com/gitterHQ/webapp/-/issues/2838#note_949966620
 const EXPIRATION_DATE = new Date('2022-05-17');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let numberofIdentitiesFilled = 0;
 async function fillGitLabIdentityRefreshTokens() {
   // We have 96231 `gitlab` provider identities in the production database as of 2022-05-17
   const cursor = Identity.find({
@@ -33,48 +41,65 @@ async function fillGitLabIdentityRefreshTokens() {
     .batchSize(BATCH_SIZE)
     .cursor();
 
-  //console.log('explain', cursor.getQuery());
-
   const iterable = iterableFromMongooseCursor(cursor);
 
-  let requests = [];
-  function drainRequests() {
-    //Identity.bulkWrite(requests);
-    requests = [];
+  let bulk = Identity.collection.initializeUnorderedBulkOp();
+  let requestCount = 0;
+  async function drainRequests() {
+    if (requestCount === 0) {
+      logger.info('Skipping this request drain since no requests to make');
+      return;
+    }
+
+    // Write a dot to the console to let them know that the script is still chugging successfully
+    rl.write('.');
+    // Do the bulk write to the database to update everything
+    await new Promise((resolve, reject) => {
+      bulk.execute(err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+    // Re-init
+    bulk = Identity.collection.initializeUnorderedBulkOp();
+    requestCount = 0;
   }
 
   for await (let identityToMigrate of iterable) {
-    //await new Promise(resolve => setTimeout(resolve, 20 * 1000));
-
-    console.log('identityToMigrate', identityToMigrate);
-    requests.push({
-      updateOne: {
-        filter: { _id: identityToMigrate._id },
-        update: {
-          $set: {
-            // Set an expiration we know to refresh later when that code is introduced.
-            // See https://gitlab.com/gitterHQ/webapp/-/merge_requests/2283
-            accessTokenExpires: EXPIRATION_DATE,
-            // Copy(migrate) the `accessTokenSecret` field to the `refreshToken` field. For some
-            // reason, previously, we were writing the refreshToken to the
-            // accessTokenSecret field.
-            refreshToken: identityToMigrate.accessTokenSecret
-          },
-          $unset: {
-            // Now that the field is copied above, clear it out
-            accessTokenSecret: ''
-          }
-        }
+    bulk.find({ _id: identityToMigrate._id }).updateOne({
+      $set: {
+        // Set an expiration we know to refresh later when that code is introduced.
+        // See https://gitlab.com/gitterHQ/webapp/-/merge_requests/2283
+        accessTokenExpires: EXPIRATION_DATE,
+        // Copy(migrate) the `accessTokenSecret` field to the `refreshToken` field. For some
+        // reason, previously, we were writing the refreshToken to the
+        // accessTokenSecret field.
+        refreshToken: identityToMigrate.accessTokenSecret
+      },
+      $unset: {
+        // Now that the field is copied above, clear it out
+        accessTokenSecret: ''
       }
     });
 
-    // Execute N write/update requests than re-init
-    if (requests.length >= BATCH_SIZE) {
-      drainRequests();
+    numberofIdentitiesFilled++;
+    requestCount++;
+
+    // Execute N write/update requests then re-init
+    if (requestCount >= BATCH_SIZE) {
+      await drainRequests();
     }
   }
 
-  drainRequests();
+  // After we're done iterating, make sure to drain the leftover requests
+  await drainRequests();
+
+  // If we're done filling, write a newline so the next log doesn't appear
+  // on the same line as the .....
+  rl.write('\n');
 }
 
 (async () => {
@@ -84,10 +109,11 @@ async function fillGitLabIdentityRefreshTokens() {
     logger.info('Connected to Mongo ✅');
     logger.info(`Filling GitLab identities with refresh tokens...`);
     await fillGitLabIdentityRefreshTokens();
-    logger.info(`Done filling ✅`);
+    logger.info(`Done after filling ${numberofIdentitiesFilled} identities ✅`);
   } catch (err) {
-    logger.info('Error', err.stack);
+    logger.info('Error while filling', err.stack);
   } finally {
+    rl.close();
     shutdown.shutdownGracefully();
   }
 })();
