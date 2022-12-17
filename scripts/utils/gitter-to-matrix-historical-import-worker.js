24224b687fbbb7898392837253e305da06e09906
Eric Eastwood <contact@ericeastwood.com>
2022-12-16 18:37:09 -0600

    Add worker index option to be able to parallize across processes and different servers

--- scripts/utils/gitter-to-matrix-historical-import-worker.js
+++ scripts/utils/gitter-to-matrix-historical-import-worker.js
@@ -36,18 +36,81 @@ const opts = require('yargs')
     required: true,
     description: 'Number of rooms to process at once'
   })
+  // Worker index option to only process rooms which evenly divide against
+  // that index (partition) (make sure to update the `laneStatusFilePath` to be unique from other
+  // workers)
+  .option('worker-index', {
+    type: 'number',
+    description:
+      '1-based index of the worker (should be unique across all workers) to only process the subset of rooms where the ID evenly divides (partition). If not set, this should be the only worker across the whole environment.'
+  })
+  .option('worker-total', {
+    type: 'number',
+    description:
+      'The total number of workers. We will partition based on this number `(id % workerTotal) === workerIndex ? doWork : pass`'
+  })
+  // If one worker options is set, the other one should be set as well
+  .implies('worker-index', 'worker-total')
+  .implies('worker-total', 'worker-index')
   .option('uri-deny-pattern', {
     type: 'string',
     required: false,
     description: 'The regex filter to match against the room lcUri'
   })
-  // TODO: Add worker index option to only process rooms which evenly divide against
-  // that index (partition) (make sure to update the `laneStatusFilePath` to be unique from other
-  // workers)
   .help('help')
   .alias('help', 'h').argv;
 
 const roomUriDenyFilterRegex = opts.uriDenyPattern ? new RegExp(opts.uriDenyPattern, 'i') : null;
+
+if (opts.workerIndex && opts.workerIndex <= 0) {
+  throw new Error(`opts.workerIndex=${opts.workerIndex} must start at 1`);
+}
+if (opts.workerIndex && opts.workerIndex > opts.workerTotal) {
+  throw new Error(
+    `opts.workerIndex=${opts.workerIndex} can not be higher than opts.workerTotal=${opts.workerTotal}`
+  );
+}
+
+// Handles numbers bigger than 64-bit
+function hexToByteArray(hexInput) {
+  // For each hex character, convert it to it's binary form
+  const binaryForCharList = hexInput.split('').map(hexChar => {
+    const decimalChar = parseInt(hexChar.toUpperCase(), 16);
+
+    if (Number.isNaN(decimalChar)) {
+      throw new Error(`Unexpected character ${hexChar} that is not hex`);
+    }
+
+    return decimalChar.toString(2).padStart(4, '0');
+  });
+
+  // Turn it into one big long binary string
+  const binaryString = binaryForCharList.join('');
+
+  // Convert into a byte list
+  const byteArray = binaryString.match(/.{8}/g);
+
+  return byteArray;
+}
+
+function splitMongoObjectIdIntoPieces(objectId) {
+  const stringifiedObjectId = String(objectId);
+  // We can't just `parseInt(objectId, 16)` because the number is bigger than 64-bit and we
+  // will lose precision
+  const byteArray = hexToByteArray(stringifiedObjectId);
+
+  // The 12-byte ObjectId consists of:
+  return {
+    // A 4-byte timestamp, representing the ObjectId's creation, measured in seconds
+    // since the Unix epoch.
+    timestampSecondsPiece: parseInt(byteArray.slice(0, 4).join(''), 2),
+    // A 5-byte random value generated once per process. This random value is unique to
+    // the machine and process.
+    randomValuePerProcess: parseInt(byteArray.slice(4, 9).join(''), 2),
+    // A 3-byte incrementing counter, initialized to a random value.
+    incrementingValue: parseInt(byteArray.slice(9, 12).join(''), 2)
+  };
+}
 
 const concurrentQueue = new ConcurrentQueue({
   concurrency: opts.concurrency,
@@ -125,18 +188,41 @@ async function exec() {
 
   await concurrentQueue.processFromGenerator(
     gitterRoomStreamIterable,
+    // Room filter
     gitterRoom => {
+      const gitterRoomId = gitterRoom.id || gitterRoom._id;
+
+      // We should not process any room that matches this room URI deny filter,
       if (roomUriDenyFilterRegex) {
+        // A ONE_TO_ONE room won't have a `lcUri` so we need to protect against that.
         const didMatchDenyRegex =
           gitterRoom.lcUri && gitterRoom.lcUri.match(roomUriDenyFilterRegex);
-        return !didMatchDenyRegex;
+        if (didMatchDenyRegex) {
+          return false;
+        }
+        // Otherwise fall-through to the next filter
       }
+
+      // If we're in worker mode, only process a sub-section of the roomID's.
+      // We partition based on part of the Mongo ObjectID.
+      if (opts.workerIndex && opts.workerTotal) {
+        // Partition based on the incrementing value part of the Mongo ObjectID. We
+        // can't just `parseInt(objectId, 16)` because the number is bigger than 64-bit
+        // (12 bytes is 96 bits) and we will lose precision
+        const { incrementingValue } = splitMongoObjectIdIntoPieces(gitterRoomId);
+
+        const shouldBeProcessedByThisWorker =
+          incrementingValue % opts.workerTotal === opts.workerIndex - 1;
+        return shouldBeProcessedByThisWorker;
+      }
+
       return true;
     },
+    // Process function
     async ({ value: gitterRoom, laneIndex }) => {
       const gitterRoomId = gitterRoom.id || gitterRoom._id;
 
-      // Track some meta info so we can display a nice UI around what's happening
+      // Track some meta info so we can display a nice UI in the dashboard around what's happening
       const numTotalMessagesInRoom = await mongooseUtils.getEstimatedCountForId(
         persistence.ChatMessage,
         'toTroupeId',
