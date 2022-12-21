31080f2a0be87b1410d2f9695ce0a77ebc31da33
Eric Eastwood <contact@ericeastwood.com>
2022-12-20 23:22:55 -0600

    Add a way to reset a room by ID (for ONE_TO_ONE)

--- scripts/utils/reset-matrix-room-bridging.js
+++ scripts/utils/reset-matrix-room-bridging.js
@@ -11,17 +11,24 @@ const troupeService = require('gitter-web-rooms/lib/troupe-service');
 const installBridge = require('gitter-web-matrix-bridge');
 const matrixBridge = require('gitter-web-matrix-bridge/lib/matrix-bridge');
 const MatrixUtils = require('gitter-web-matrix-bridge/lib/matrix-utils');
+const { assert } = require('console');
 
 const matrixUtils = new MatrixUtils(matrixBridge);
 
 const opts = require('yargs')
+  .option('id', {
+    description: 'ID of the Gitter room to reset Matrix bridging status'
+  })
   .option('uri', {
     alias: 'u',
-    required: true,
     description: 'URI of the Gitter room to reset Matrix bridging status'
   })
   .help('help')
   .alias('help', 'h').argv;
+
+if (!opts.id && !opts.uri) {
+  throw new Error('--id or --uri are required');
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -33,7 +40,13 @@ async function exec() {
   logger.info('Setting up Matrix bridge');
   await installBridge();
 
-  const gitterRoom = await troupeService.findByUri(opts.uri);
+  let gitterRoom;
+  if (opts.id) {
+    gitterRoom = await troupeService.findById(opts.id);
+  } else if (opts.uri) {
+    gitterRoom = await troupeService.findByUri(opts.uri);
+  }
+  assert('gitterRoom', `Gitter room not found for opts.id=${opts.id} or opts.uri=${opts.uri}`);
   const gitterRoomId = gitterRoom.id || gitterRoom._id;
 
   const matrixRoomId = await matrixUtils.getOrCreateMatrixRoomByGitterRoomId(gitterRoomId);
@@ -58,21 +71,27 @@ async function exec() {
     );
   });
 
-  // Delete the canonical primary alias from the room directory (and whatever aliases were set)
-  await matrixUtils.deleteRoomAliasesForMatrixRoomId(matrixRoomId);
+  if (!gitterRoom.oneToOne) {
+    // Delete the canonical primary alias from the room directory (and whatever aliases were set)
+    await matrixUtils.deleteRoomAliasesForMatrixRoomId(matrixRoomId);
+    console.log(`Removed room aliases for matrixRoomId=${matrixRoomId}`);
+  }
 
   // Delete bridged chat message entries since the given date
   await persistence.MatrixBridgedChatMessage.remove({
     matrixRoomId
   }).exec();
+  console.log(`Removed bridged chat messages in matrixRoomId=${matrixRoomId}`);
 
   // Shutdown the room so people don't get confused about it
   await matrixUtils.shutdownMatrixRoom(matrixRoomId);
+  console.log(`Shut down room matrixRoomId=${matrixRoomId}`);
 
   // Delete the bridged room entry
   await persistence.MatrixBridgedRoom.remove({
     matrixRoomId
   }).exec();
+  console.log(`Remove bridged room entry for matrixRoomId=${matrixRoomId}`);
 }
 
 exec()
