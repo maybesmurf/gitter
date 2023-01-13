d7bb516e6fb2f1754772c646738e1aa19615ad8b
Eric Eastwood <contact@ericeastwood.com>
2023-01-13 16:32:08 -0600

    Special username for ghosts

--- modules/matrix-bridge/lib/get-mxid-for-gitter-user.js
+++ modules/matrix-bridge/lib/get-mxid-for-gitter-user.js
@@ -4,6 +4,15 @@ const config = env.config;
 const serverName = config.get('matrix:bridge:serverName');
 
 function getMxidForGitterUser(gitterUser) {
+  // Our goal is to remove the `~` from the ghosted username because it will get escaped by the
+  // bridging libraries otehrwise and cause MXID mismatches and then claims we haven't
+  // registered the user yet.
+  //
+  // Ghosted usernames look like `@ghost=7e5f762ffe986e461e663059f0-5f762ffe986e461e663059f0:gitter.im`
+  if (gitterUser.username.startsWith('ghost~')) {
+    return `@ghost-${gitterUser.id}:${serverName}`;
+  }
+
   const mxid = `@${gitterUser.username.toLowerCase()}-${gitterUser.id}:${serverName}`;
   return mxid;
 }
