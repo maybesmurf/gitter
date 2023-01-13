43bbf360954b135597a57863e4271c7cf9be515d
Eric Eastwood <contact@ericeastwood.com>
2023-01-13 15:17:53 -0600

    WIP

--- modules/matrix-bridge/lib/gitter-to-matrix-historical-import.js
+++ modules/matrix-bridge/lib/gitter-to-matrix-historical-import.js
@@ -180,6 +180,12 @@ async function _getOrCreateMatrixUserByGitterUserIdCached(gitterUserId) {
   }
 
   const matrixId = await matrixUtils.getOrCreateMatrixUserByGitterUserId(gitterUserId);
+  console.log(
+    '_getOrCreateMatrixUserByGitterUserIdCached gitterUserId',
+    gitterUserId,
+    'matrixId',
+    matrixId
+  );
 
   gitterUserIdToMxidCache.set(cacheKey, matrixId);
 
