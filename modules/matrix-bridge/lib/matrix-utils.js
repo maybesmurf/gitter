c8856f224e1ddf71be617b23ad7fa8567353c2f2
Eric Eastwood <contact@ericeastwood.com>
2023-01-31 10:23:29 +0000

    Follow-up fixes to bridging admin power levels MR !2343
    (https://gitlab.com/gitterHQ/webapp/-/merge_requests/2343)
    
     1. When someone joins the room, if we add them as an admin of the Matrix room, also add them as an admin of the historical Matrix room
     1. Make sure to call `cleanupAdminsInMatrixRoomIdAccordingToGitterRoomId(...)` alongside `addAdminsInMatrixRoomIdAccordingToGitterRoomId(...)` when we `ensureCorrectRoomState(...)`
     1. Refactor to use `ensureStateEvent(...)` instead of `ensureStateEventAsMxid(...)` as the bridge user

--- modules/matrix-bridge/lib/matrix-utils.js
+++ modules/matrix-bridge/lib/matrix-utils.js
@@ -36,7 +36,7 @@ const store = require('./store');
 
 const DB_BATCH_SIZE_FOR_ROOM_MEMBERSHIP = 100;
 
-const serverName = config.get('matrix:bridge:serverName');
+const configuredServerName = config.get('matrix:bridge:serverName');
 // The bridge user we are using to interact with everything on the Matrix side
 const matrixBridgeMxidLocalpart = config.get('matrix:bridge:matrixBridgeMxidLocalpart');
 // The Gitter user we are pulling profile information from to populate the Matrix bridge user profile
@@ -269,19 +269,13 @@ class MatrixUtils {
       return;
     }
 
-    await this.ensureStateEventAsMxid(
-      // undefined will give us the bridgeIntent
-      undefined,
-      matrixRoomId,
-      'm.room.power_levels',
-      {
-        ...currentPowerLevelContent,
-        users: {
-          ...(currentPowerLevelContent.users || {}),
-          [mxid]: ROOM_ADMIN_POWER_LEVEL
-        }
+    await this.ensureStateEvent(matrixRoomId, 'm.room.power_levels', {
+      ...currentPowerLevelContent,
+      users: {
+        ...(currentPowerLevelContent.users || {}),
+        [mxid]: ROOM_ADMIN_POWER_LEVEL
       }
-    );
+    });
   }
 
   async removeAdminFromMatrixRoomId({ mxid, matrixRoomId }) {
@@ -306,16 +300,10 @@ class MatrixUtils {
     // Then delete the person that should no longer be an admin
     delete newPowerLevelUsersMap[mxid];
 
-    await this.ensureStateEventAsMxid(
-      // undefined will give us the bridgeIntent
-      undefined,
-      matrixRoomId,
-      'm.room.power_levels',
-      {
-        ...currentPowerLevelContent,
-        users: newPowerLevelUsersMap
-      }
-    );
+    await this.ensureStateEvent(matrixRoomId, 'm.room.power_levels', {
+      ...currentPowerLevelContent,
+      users: newPowerLevelUsersMap
+    });
   }
 
   // Loop through all of the room admins listed in the Matrix power levels and
@@ -327,16 +315,14 @@ class MatrixUtils {
     const gitterRoom = await troupeService.findById(gitterRoomId);
     assert(gitterRoom);
 
-    const configuredServerName = this.matrixBridge.opts.domain;
-    assert(configuredServerName);
-
     const bridgeIntent = this.matrixBridge.getIntent();
     const currentPowerLevelContent = await bridgeIntent.getStateEvent(
       matrixRoomId,
       'm.room.power_levels'
     );
+    const powerLevelUserMap = (currentPowerLevelContent && currentPowerLevelContent.users) || {};
 
-    for (const mxid of Object.keys(currentPowerLevelContent.users || {})) {
+    for (const mxid of Object.keys(powerLevelUserMap)) {
       // Skip any MXID's that aren't from our own server (gitter.im)
       const { serverName } = parseGitterMxid(mxid) || {};
       if (serverName !== configuredServerName) {
@@ -523,7 +509,7 @@ class MatrixUtils {
     const roomAlias = getCanonicalAliasForGitterRoomUri(gitterRoom.uri);
     await this.ensureRoomAlias(matrixRoomId, roomAlias);
     // Add another alias for the room ID
-    await this.ensureRoomAlias(matrixRoomId, `#${gitterRoomId}:${serverName}`);
+    await this.ensureRoomAlias(matrixRoomId, `#${gitterRoomId}:${configuredServerName}`);
     // Add a lowercase alias if necessary
     if (roomAlias.toLowerCase() !== roomAlias) {
       await this.ensureRoomAlias(matrixRoomId, roomAlias.toLowerCase());
@@ -646,6 +632,10 @@ class MatrixUtils {
     });
 
     // Add the Gitter room admins to the power levels to be able to self-manage later
+    await this.cleanupAdminsInMatrixRoomIdAccordingToGitterRoomId({
+      matrixRoomId,
+      gitterRoomId
+    });
     await this.addAdminsInMatrixRoomIdAccordingToGitterRoomId({
       matrixRoomId,
       gitterRoomId
@@ -830,7 +820,7 @@ class MatrixUtils {
   }
 
   getMxidForMatrixBridgeUser() {
-    const mxid = `@${matrixBridgeMxidLocalpart}:${serverName}`;
+    const mxid = `@${matrixBridgeMxidLocalpart}:${configuredServerName}`;
     return mxid;
   }
 
