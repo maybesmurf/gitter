2743936f42264066e6952359f2297f5520773827
Eric Eastwood <contact@ericeastwood.com>
2023-02-04 03:20:10 +0000

    Fix `logger.warning` -> `logger.warn` typo

--- scripts/utils/ensure-existing-bridged-matrix-room-up-to-date.js
+++ scripts/utils/ensure-existing-bridged-matrix-room-up-to-date.js
@@ -128,7 +128,7 @@ async function run() {
           // This is very bad and hacky but `matrix-appservice-bridge` gives us no other clues of this specific problem
           err.message.startsWith('Cannot ensure client has power level for event')
         ) {
-          logger.warning(
+          logger.warn(
             `Unable to update matrixRoomId=${matrixRoomId} (bridged to gitterRoomId=${gitterRoomId}) because we don't have permission in that room. Since this room is bridged to a non-gitter.im room, we can't do anything more to help it.`,
             {
               exception: err
